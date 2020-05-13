c2641a19cc5ee963993fefeca2edcc1b55687968
Adrián Cantón Fernández <adrcanfer@alum.us.es>
2020-05-13 08:38:27 +0200

    Added project

---
+++ src/main/java/com/adrcanfer/clrunner/ClrunnerApplication.java
@@ -0,0 +1,13 @@
+package com.adrcanfer.clrunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClrunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClrunnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adrcanfer/clrunner/ClrunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrcanfer.clrunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClrunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
